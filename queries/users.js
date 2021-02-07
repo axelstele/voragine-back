0f7905bb83145352df830d6c43e96acb1f234a0f
axelstele <axel.stele@gmail.com>
2021-02-07 19:02:28 -0300

    login API

--- queries/users.js
+++ queries/users.js
@@ -1,14 +1,30 @@
+const bcrypt = require('bcrypt');
 const { database } = require('../database');
 
-const get = (request, response) => {
-  database.query('SELECT * FROM "voragine"."users" ORDER BY id ASC', (error, results) => {
+const login = (request, response) => {
+  const { body } = request;
+  database.query(`SELECT * FROM "${process.env.LOCAL_DATABASE_SCHEMA}"."users" as u WHERE email = '${body.email}'`, (error, results) => {
     if (error) {
       throw error;
     }
-    response.status(200).json(results.rows);
+    if (!results.rows.length) {
+      return response.status(404).send({
+        message: 'User not found.',
+      });
+    }
+    const user = results.rows[0];
+    bcrypt.compare(body.password, user.password, (err, res) => {
+      if (err) {
+        return response.status(500).send({ message: 'An error occurred. Try again later.' });
+      }
+      if (res) {
+        return response.status(200).send({ message: 'Logged in.' });
+      }
+      return response.status(401).send({ message: 'User or password incorrect.' });
+    });
   });
 };
 
 module.exports = {
-  get,
+  login,
 };
