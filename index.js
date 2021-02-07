eeae7f321905bc59ecb2f79d924dd8ae522308cc
axelstele <axel.stele@gmail.com>
2021-02-07 19:25:57 -0300

    test fix Heroku cors

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const users = require('./queries/users');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 app.use(
   bodyParser.urlencoded({
     extended: true,
   }),
 );
 app.use(bodyParser.json());
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
