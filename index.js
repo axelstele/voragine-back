818fb31ff67c4baadb69b763835cdd95bffb9ada
axelstele <axel.stele@gmail.com>
2021-02-08 18:51:11 -0300

    create products API

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
+require('./cloudinary-config');
+
 const users = require('./queries/users');
+const products = require('./queries/products');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -15,11 +20,17 @@ app.use(
 );
 app.use(bodyParser.json());
 
+const fileUpload = multer();
+
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
+// users
 app.post('/login', (request, response) => users.login(request, response));
+
+// products
+app.post('/products', fileUpload.array('images'), (request, response) => products.create(request, response));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
---
+++ queries/products.js
@@ -0,0 +1,57 @@
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+const { database } = require('../database');
+
+const streamUpload = (file) => new Promise((resolve, reject) => {
+  const stream = cloudinary.uploader.upload_stream(
+    (error, result) => {
+      if (result) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    },
+  );
+
+  streamifier.createReadStream(file.buffer).pipe(stream);
+});
+
+const create = (request, response) => {
+  try {
+    const {
+      body: {
+        title, description, price, category, quantity,
+      },
+      files,
+    } = request;
+    database.query(`
+      INSERT INTO "${process.env.DATABASE_SCHEMA}"."products" VALUES (default, '${title}', '${description}', ${price}, ${category}, ${quantity})`, (error) => {
+      if (error) {
+        throw error;
+      }
+      database.query(`SELECT MAX(id) as lastid FROM "${process.env.DATABASE_SCHEMA}"."products"`, (error2, results) => {
+        if (error2) {
+          throw error2;
+        }
+        const { lastid } = results.rows[0];
+        files.map(async (file) => {
+          const { url } = await streamUpload(file);
+          database.query(`INSERT INTO "${process.env.DATABASE_SCHEMA}"."images_products" VALUES (default, ${lastid}, '${url}')`, (error3) => {
+            if (error3) {
+              throw error3;
+            }
+          });
+        });
+        return response.status(201).send({
+          message: 'Product added.',
+        });
+      });
+    });
+  } catch (error) {
+    response.status(500).send({ message: 'An error occurred. Try again later.' });
+  }
+};
+
+module.exports = {
+  create,
+};
